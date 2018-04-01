e9216006dcff2c9169210fab3fc43119b709d0bb
Luis Arteiro <lucho14garfield@gmail.com>
2018-04-01 23:07:17 +0100

    Adding test project

---
+++ Bulls and Cows/BullCowGameTest/TestFBullCowGame.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../BullCowGame/FBullCowGame.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BullCowGameTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+	};
+}
